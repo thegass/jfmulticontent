c4ce6fd48bc9923fe622ad247363887e2f8a8e7a
Thomas Löffler <loeffler@spooner-web.de>
2015-12-23 10:30:42 +0100

    Update ext_autoload.php
    Due to wrong writing the class is not found in TYPO3 <6

--- ext_autoload.php
+++ ext_autoload.php
@@ -4,5 +4,5 @@ $extensionPath = t3lib_extMgm::extPath('jfmulticontent');
 
 return array(
 	'tx_jfmulticontent_pagerenderer'  => $extensionPath . 'lib/class.tx_jfmulticontent_pagerenderer.php',
-	'tx_jfmulticontent_itemsProcFunc' => $extensionPath . 'lib/class.tx_jfmulticontent_itemsProcFunc.php',
+	'tx_jfmulticontent_itemsprocfunc' => $extensionPath . 'lib/class.tx_jfmulticontent_itemsProcFunc.php',
 );
